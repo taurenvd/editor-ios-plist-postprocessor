2300c9058d5b229cf8b6fd60d23153d439b13144
[Partner] Valerii Dril <v.dril@room8studio.com>
2021-06-30 19:32:15 +0300

    serialization fix

--- Editor/iOSPlistScriptable.cs
+++ Editor/iOSPlistScriptable.cs
@@ -12,12 +12,27 @@ using UnityEditor.iOS.Xcode;
 [CreateAssetMenu(fileName = "IOSPlistScriptable")]
 public class iOSPlistScriptable : ScriptableObject
 {
-    [SerializeField] public string[] plist_items_to_remove = new[] { "UIApplicationExitsOnSuspend" };
-    [SerializeField] public PlistDictionary plist_items_to_add = new PlistDictionary { { "ITSAppUsesNonExemptEncryption", "NO" } };
+    [SerializeField] public string[] plist_items_to_remove = new[] {"UIApplicationExitsOnSuspend"};
+    [SerializeField] public List<PlistEntry> plist_items_to_add = new List<PlistEntry>() {new PlistEntry("ITSAppUsesNonExemptEncryption", "NO")};
 
 }
 
 [Serializable]
-public class PlistDictionary : Dictionary<string, string> { }
+public class PlistEntry
+{
+    [SerializeField] string _key;
+    [SerializeField] string _value;
+
+    public string Key => _key;
+    public string Value => _value;
+
+    public PlistEntry() { }
+
+    public PlistEntry(string key, string value)
+    {
+        _key = key;
+        _value = value;
+    }
+}
 
 #endif
